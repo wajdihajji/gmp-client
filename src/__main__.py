56496736578bfc564eda8f7c72f0f84d3a4ab8fb
Wajdi Hajji <wh330@cam.ac.uk>
2020-12-03 17:45:37 +0000

    Restructuring main function

--- src/__main__.py
+++ src/__main__.py
@@ -19,7 +19,7 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 
-# This decorator can be applied to
+# logging decorator for schedule jobs
 def with_logging(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -30,12 +30,6 @@ def with_logging(func):
     return wrapper
 
 
-gmp_client = GMPClient()
-db_connection = create_db_connection()
-populate_hosts_table(db_connection, permutation_elts=8)
-
-
-@with_logging
 def job_init(gmp_client):
     """Runs scan and discovery initialisation."""
     initialise_scan(gmp_client)
@@ -45,25 +39,37 @@ def job_init(gmp_client):
 @with_logging
 def job_run_discovery(gmp_client, db_connection):
     """Runs host discovery."""
+    # Get the hosts that have not been selected_for discovery or seen up,
+    # not selected for scan and not yet scanned.
     sub_hosts = get_hosts(db_connection, [0], [0], [0], [0], num_records=1024)
     run_discovery(gmp_client, db_connection, sub_hosts)
-    time.sleep(10)
 
 
 @with_logging
 def job_run_scan(gmp_client, db_connection):
     """Runs scan."""
-    sub_hosts = get_hosts(db_connection, [0], [0], [0], [0], num_records=1024)
+    # Get the hosts that have been seen up, not selected for scan and not yet scanned.
+    sub_hosts = get_hosts(db_connection, [0, 1], [1], [0], [0], num_records=1024)
     run_scan(gmp_client, db_connection, sub_hosts)
-    time.sleep(30)
-
-
-# This schedule simply means run job_run_discovery first then job_run_scan and do it infinitely.
-schedule.every(1).seconds.do(job_run_discovery, gmp_client=gmp_client, db_connection=db_connection)
-schedule.every(2).seconds.do(job_run_scan, gmp_client=gmp_client, db_connection=db_connection)
 
 
 if __name__ == '__main__':
+    gmp_client = GMPClient()
+    db_connection = create_db_connection()
+    populate_hosts_table(db_connection, permutation_elts=4)
+
+    job_init(gmp_client)
+
+    # The following schedule simply means run job_run_discovery then job_run_scan and do that infinitely.
+    schedule.every(15).seconds.do(
+        job_run_discovery, gmp_client=gmp_client, db_connection=db_connection)
+    schedule.every(15).seconds.do(
+        job_run_scan, gmp_client=gmp_client, db_connection=db_connection)
+
+    # while True:
+    #     job_run_discovery(gmp_client, db_connection)
+    #     job_run_scan(gmp_client, db_connection)
+    #     time.sleep(10)
     while True:
         schedule.run_pending()
         time.sleep(1)
