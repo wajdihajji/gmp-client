b7f6254ad61523914eb65105459b25912dc829c6
Wajdi Hajji <wh330@cam.ac.uk>
2021-01-13 10:04:41 +0000

    Update main function

--- src/__main__.py
+++ src/__main__.py
@@ -11,7 +11,8 @@ import schedule
 from fps.client import GMPClient
 from fps.tasks import (initialise_discovery, initialise_scan, run_discovery,
                        run_scan, update_host_attribute)
-from fps.utils import create_db_connection, get_hosts, populate_hosts_table
+from fps.utils import (create_pg_conn, create_sqlite_conn, get_hosts,
+                       import_hosts, populate_hosts_table, create_hosts_table)
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -31,53 +32,53 @@ def with_logging(func):
 
 
 @with_logging
-def job_populate_hosts_table(db_connection):
+def job_populate_hosts_table(db_conn):
     """Loads the host files in `./data` into the SQLite table `hosts`."""
-    populate_hosts_table(db_connection)
+    populate_hosts_table(db_conn)
 
 
 @with_logging
-def job_run_discovery(gmp_client, db_connection):
+def job_run_discovery(gmp_client, db_conn):
     """Runs host discovery."""
     # Get the hosts that have not been selected for discovery or seen up,
     # not selected for scan and not yet scanned.
     sub_hosts = get_hosts(
-        db_connection, [0], [0], [0], [0], num_records=config.getint('DISCOVERY', 'max_num_hosts'))
-    run_discovery(gmp_client, db_connection, sub_hosts)
+        db_conn, [0], [0], [0], [0], num_records=config.getint('DISCOVERY', 'max_num_hosts'))
+    run_discovery(gmp_client, db_conn, sub_hosts)
 
 
 @with_logging
-def job_run_scan(gmp_client, db_connection):
+def job_run_scan(gmp_client, db_conn):
     """Runs scan."""
     # Get the hosts that have been seen up, not selected for scan and not yet scanned.
     sub_hosts = get_hosts(
-        db_connection, [0, 1], [1], [0], [0], num_records=config.getint('SCAN', 'max_num_hosts'))
+        db_conn, [0, 1], [1], [0], [0], num_records=config.getint('SCAN', 'max_num_hosts'))
     for host in sub_hosts:
-        update_host_attribute(db_connection, 'selected_for_scan', 1, host)
-    run_scan(gmp_client, db_connection, sub_hosts)
+        update_host_attribute(db_conn, 'selected_for_scan', 1, host)
+    run_scan(gmp_client, db_conn, sub_hosts)
 
 
 if __name__ == '__main__':
     gmp_client = GMPClient()
-    db_connection = create_db_connection()
-    populate_hosts_table(db_connection)
+    sqlite_conn = create_sqlite_conn()
+    pg_conn = create_pg_conn()
+    create_hosts_table(sqlite_conn)
+    import_hosts(pg_conn, sqlite_conn)
     initialise_scan(gmp_client)
     initialise_discovery(gmp_client)
 
     # Run job_populate_hosts_table every day at 00:01
     schedule.every().day.at("00:01").do(
-        job_populate_hosts_table, db_connection=db_connection)
+        job_populate_hosts_table, db_connection=sqlite_conn)
 
     discovery_freq = config.getint('DISCOVERY', 'frequency')
     scan_freq = config.getint('SCAN', 'frequency')
 
     # Run job_run_discovery every `discovery_freq` minutes
-    schedule.every(discovery_freq).minutes.do(
-        job_run_discovery, gmp_client=gmp_client, db_connection=db_connection)
+        job_run_discovery, gmp_client=gmp_client, db_conn=sqlite_conn)
 
     # Run job_run_scan every `scan_freq` minutes
-    schedule.every(scan_freq).minutes.do(
-        job_run_scan, gmp_client=gmp_client, db_connection=db_connection)
+        job_run_scan, gmp_client=gmp_client, db_conn=sqlite_conn)
 
     while True:
         schedule.run_pending()
