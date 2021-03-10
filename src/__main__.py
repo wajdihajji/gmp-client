d08cd8780c3f8c9468af1e342e006569d6f9a9f1
Wajdi Hajji <wh330@cam.ac.uk>
2021-03-10 11:28:15 +0000

    Defined init and scheduled scan tasks

--- src/__main__.py
+++ src/__main__.py
@@ -11,8 +11,9 @@ import schedule
 from fps.client import GMPClient
 from fps.tasks import (initialise_discovery, initialise_scan, run_discovery,
                        run_scan, update_host_attribute)
-from fps.utils import (create_pg_conn, create_sqlite_conn, get_hosts,
-                       import_hosts, populate_hosts_table, create_hosts_table)
+from fps.utils import (create_hosts_table, create_pg_conn, create_sqlite_conn,
+                       del_hosts_by_day, get_hosts, import_hosts,
+                       populate_hosts_table)
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -69,33 +70,50 @@ def job_run_scan(gmp_client, sqlite_conn, pg_conn):
 
 if __name__ == '__main__':
     gmp_client = GMPClient()
-
     sqlite_conn = create_sqlite_conn()
     pg_conn = create_pg_conn()
-
-    create_hosts_table(sqlite_conn)
-
-    initialise_scan(gmp_client)
-    initialise_discovery(gmp_client)
-
-    # Import hosts at the initialisation step
-    job_import_hosts(sqlite_conn, pg_conn)
 
     discovery_freq = config.getint('DISCOVERY', 'frequency')
     import_hosts_freq = config.getint('DISCOVERY', 'hosts_import_frequency')
     scan_freq = config.getint('SCAN', 'frequency')
 
-    # Import hosts at `import_hosts_freq` frequency
+    # Initialisation steps:
+    # 1. create sqlite hosts table
+    create_hosts_table(sqlite_conn)
+
+    # 2. create scanners and their dependencies
+    initialise_scan(gmp_client)
+
+    # 3. create discovery scanner and its dependency
+    initialise_discovery(gmp_client)
+
+    # 4. import hosts from either csv file or pg database
+    job_import_hosts(sqlite_conn, pg_conn)
+
+    # 5. delete next day hosts in the SQL database
+    # This to prepare for getting the next day hosts to scan
+    del_hosts_by_day(sqlite_conn)
+
+    # 6. start the first discovery task
+    job_run_discovery(gmp_client, sqlite_conn, pg_conn)
+
+    # Scheduled steps:
+    # 1. import hosts every `import_hosts_freq` minutes
     schedule.every(import_hosts_freq).minutes.do(
         job_import_hosts, sqlite_conn=sqlite_conn, pg_conn=pg_conn)
 
-    # Run job_run_discovery every `discovery_freq` minutes
-    schedule.every(discovery_freq).minutes.do(
+    # 2. run job_run_discovery every `discovery_freq` minutes
+    schedule.every(10).seconds.do(
         job_run_discovery, gmp_client=gmp_client, sqlite_conn=sqlite_conn, pg_conn=pg_conn)
 
-    # Run job_run_scan every `scan_freq` minutes
-    schedule.every(scan_freq).minutes.do(
+    # 3. run job_run_scan every `scan_freq` minutes
+    schedule.every(10).seconds.do(
         job_run_scan, gmp_client=gmp_client, sqlite_conn=sqlite_conn, pg_conn=pg_conn)
+
+    # 4. delete next day hosts at 11pm
+    schedule.every().day.at("23:00:00").do(
+        del_hosts_by_day, sqlite_conn=sqlite_conn
+    )
 
     while True:
         schedule.run_pending()
